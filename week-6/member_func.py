0373659eefc0b8f943ca776038588fac55780035
ting-yang14 <sheep870104@gmail.com>
2022-10-26 17:11:53 +0800

    use connection_pool

--- week-6/member_func.py
+++ week-6/member_func.py
@@ -1,51 +1,51 @@
-import mysql.connector
+from mysql.connector import pooling
 
-mydb = mysql.connector.connect(
+connection_pool = pooling.MySQLConnectionPool(
+    pool_name = "test",
+    pool_size = 5,
+    pool_reset_session = True,
     host = "localhost",
     user = "root",
-    password = "",
-    database = "website"
+    password = "test1234",
+    database = "website",
+    autocommit = True
 )
+connection_object = connection_pool.get_connection()
+cursor=connection_object.cursor()
+
 def add_member(name, username, password):
     # find repeat username
-    mycursor = mydb.cursor()
     sql = "SELECT username FROM member WHERE username = %s"
     adr = (username,) 
-    mycursor.execute(sql, adr)
-    myresult = mycursor.fetchall() # list of repeat username
-    if not myresult: #  list is empty, no repeat username
+    cursor.execute(sql, adr)
+    result = cursor.fetchall() # list of repeat username
+    if not result: #  list is empty, no repeat username
         # insert new member to database
         sql = "INSERT INTO member (name, username, password) VALUES (%s, %s, %s)"
         val = (name, username, password)
-        mycursor.execute(sql, val)
-        mydb.commit()
+        cursor.execute(sql, val)
         return True
     else:
         return False
 
 def check_member(username, password):
     # ckeck whether member have signup
-    mycursor = mydb.cursor()
     sql = "SELECT * FROM member WHERE username = %s AND password = %s"
     adr = (username, password) 
-    mycursor.execute(sql, adr)
-    myresult = mycursor.fetchone() # list of signin member data
-    if myresult: #  have member data
-        print(myresult)
-        return (True, myresult) # return is member and member data
+    cursor.execute(sql, adr)
+    result = cursor.fetchone() # list of signin member data
+    if result: #  have member data
+        return (True, result) # return is member and member data
     else: # signin fail
         return (False, "帳號或密碼輸入錯誤")
 
 def add_message(member_id, content):
-    mycursor = mydb.cursor()
     sql = "INSERT INTO message (member_id, content) VALUES (%s, %s)"
     val = (member_id, content)
-    mycursor.execute(sql, val)
-    mydb.commit() #
+    cursor.execute(sql, val)
 
 def get_message_content():
-    mycursor = mydb.cursor()
     sql = "SELECT member.username, message.content FROM member INNER JOIN message ON message.member_id=member.id ORDER BY message.time DESC;"
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall() 
-    return myresult # list of member name and message content 
+    cursor.execute(sql)
+    result = cursor.fetchall() 
+    return result # list of member name and message content 
