f079aaf02b89f490760f9689abc437e35c1c5973
ting-yang14 <sheep870104@gmail.com>
2022-11-01 16:53:59 +0800

    first commit

---
+++ week-7/app.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, render_template, redirect, session, jsonify
+import flask.scaffold
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
+from flask_restful import Api, Resource
+from member_func import add_member, check_member, add_message, get_message_content, get_member_info, update_member_name
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+
+@app.route("/")
+def home():
+    if "user" in session:
+        return redirect("/member")
+    return render_template("home.html")
+
+@app.route("/member")
+def member():
+    if "username" not in session:
+        return redirect("/")
+    message_content = get_message_content()
+    return render_template("member.html", name = session["name"], message_content = message_content, len = len(message_content))
+
+@app.route("/error")
+def error():
+    return render_template("error.html", msg = session["msg"])
+
+@app.route("/signup", methods = ["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        name = request.form["name"]
+        username = request.form["username"]
+        password = request.form["password"]
+        if (name != "" and username != "" and password != ""):
+            if add_member(name, username, password):
+                return render_template("home.html")
+            else:
+                session["msg"] = "帳號已經被註冊"
+                return redirect("/error?message="+session["msg"])
+        else:
+            session["msg"] = "請輸入完整資訊"
+            return redirect("/error?message="+session["msg"])
+    return redirect("/")
+
+@app.route("/signin", methods = ["GET", "POST"])
+def signin():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        check_result = check_member(username, password)
+        if check_result[0]:
+            session["id"] = check_result[1][0]
+            session["name"] = check_result[1][1]
+            session["username"] = check_result[1][2]
+            
+            return redirect("/member")
+        else:
+            session["msg"] = check_result[1]
+            return redirect("/error?message="+session["msg"])
+    return redirect("/")
+
+@app.route("/signout")
+def signout():
+    session.pop("id", None)
+    session.pop("name", None)
+    session.pop("username", None)
+    return redirect("/")
+
+@app.route("/message", methods=["POST"])
+def message():
+    member_id = session["id"]
+    content = request.form["message"]
+    if content != "":
+        add_message(member_id, content)
+    return redirect("/member")
+    
+@app.route("/api/member", methods=["GET"])
+def get():
+    if "username" in session:
+        try:
+            member = request.args.get("username", None)
+            result=get_member_info(member)
+            return jsonify({"data":{"id":result[0],"name":result[1],"username":result[2]}})
+        except:
+            return jsonify({"data":None})
+    return jsonify({"data":None})
+
+@app.route("/api/member", methods=["PATCH"])
+def patch():
+    if "username" in session:
+        try:
+            request_params=request.get_json()
+            print(request_params)
+            new_name=request_params["name"]
+            print(session["username"])
+            print(new_name)
+            update_member_name(new_name,session["username"])
+            session["name"]=new_name
+            print(session["name"])
+            return jsonify({"ok":True})
+        except:
+            return jsonify({"error":True})
+    return jsonify({"error":True})
+
+if __name__ == "__main__":
+    app.run(port = 3000, debug = True)
---
+++ week-7/member_func.py
@@ -0,0 +1,89 @@
+from mysql.connector import pooling
+
+connection_pool = pooling.MySQLConnectionPool(
+    pool_name = "test",
+    pool_size = 5,
+    pool_reset_session = True,
+    host = "localhost",
+    user = "root",
+    password = "test1234",
+    database = "website",
+    autocommit = True
+)
+
+def add_member(name, username, password):
+    # find repeat username
+    connection = connection_pool.get_connection()
+    cursor=connection.cursor()
+    sql = "SELECT username FROM member WHERE username = %s"
+    adr = (username,) 
+    cursor.execute(sql, adr)
+    result = cursor.fetchall() # list of repeat username
+    if not result: #  list is empty, no repeat username
+        # insert new member to database
+        sql = "INSERT INTO member (name, username, password) VALUES (%s, %s, %s)"
+        val = (name, username, password)
+        cursor.execute(sql, val)
+        cursor.close()
+        connection.close()
+        return True
+    else:
+        cursor.close()
+        connection.close()
+        return False
+
+def check_member(username, password):
+    # ckeck whether member have signup
+    connection = connection_pool.get_connection()
+    cursor=connection.cursor()
+    sql = "SELECT * FROM member WHERE username = %s AND password = %s"
+    adr = (username, password) 
+    cursor.execute(sql, adr)
+    result = cursor.fetchone() # list of signin member data
+    if result: #  have member data
+        cursor.close()
+        connection.close()
+        return (True, result) # result=(1, 'test2', 'test', 'test', 150, datetime.datetime(2022, 10, 17, 17, 48, 29))
+    else: # signin fail
+        cursor.close()
+        connection.close()
+        return (False, "帳號或密碼輸入錯誤")
+
+def add_message(member_id, content):
+    connection = connection_pool.get_connection()
+    cursor=connection.cursor()
+    sql = "INSERT INTO message (member_id, content) VALUES (%s, %s)"
+    val = (member_id, content)
+    cursor.execute(sql, val)
+    cursor.close()
+    connection.close()
+
+def get_message_content():
+    connection = connection_pool.get_connection()
+    cursor=connection.cursor()
+    sql = "SELECT member.username, message.content FROM member INNER JOIN message ON message.member_id=member.id ORDER BY message.time DESC;"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return result # list of member name and message content 
+
+def get_member_info(username):
+    connection = connection_pool.get_connection()
+    cursor=connection.cursor()
+    sql = "SELECT * FROM member WHERE username = %s;"
+    val = (username,)
+    cursor.execute(sql, val)
+    result = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    return result
+
+def update_member_name(new_name, username):
+    connection = connection_pool.get_connection()
+    cursor=connection.cursor()
+    sql = "UPDATE member SET name = %s WHERE username = %s;"
+    val = (new_name, username)
+    cursor.execute(sql, val)
+    cursor.close()
+    connection.close()
